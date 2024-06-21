79eb40d414a9455eae3fae44ff84749076956ecf
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-06-21 09:26:42 +0700

    add light and dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <ModeToggle />
+    </main>
+  );
 }
