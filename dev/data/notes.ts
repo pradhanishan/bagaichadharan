75bbffa536d3f64ba3282057ef24ba07da83832e
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-06-22 14:40:24 +0700

    add notes

---
+++ dev/data/notes.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ * Employee
+ *
+ * Expenses ko data entry garnu paryo.
+ * ---- employee le bill record garda khera:
+ * --------> kati uthyo.
+ * --------> kati tiryo.
+ *          :debtors
+ * Nepali calendar hunu paryo.
+ * Stock.
+ *
+ * ----> unit of measurement.
+ * -------> supplier sanga ko debt track garnu.
+ *
+ */
